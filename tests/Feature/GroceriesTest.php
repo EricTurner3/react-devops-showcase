6d987c69fcfcf879bd804b1b120a5dd0e922c723
Eric Turner <me@ericturner.it>
2021-05-25 09:48:42 -0400

    Laravel API Feature Test

---
+++ tests/Feature/GroceriesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GroceriesTest extends TestCase
+{
+    /**
+     * @test /api/groceries 
+     *
+     * @return void
+     */
+     public function test_get_groceries()
+     {
+         $response = $this->getJson('/api/groceries');
+ 
+         $response->assertStatus(200) // ensure we get a 200 - Success Status code
+                  -> assertJsonStructure(['*' => [
+                      'id',
+                      'item',
+                      'is_completed',
+                      'created_at',
+                      'updated_at'
+                  ]]);    // ensure we get a json response in the following format
+     }   
+}
